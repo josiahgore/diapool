da0c3edff92a970be42c6588de28c3b9d961d436
Josiah Gore <josiahgore@gmail.com>
2013-02-05 11:42:35 -0500

    inital commit of rails generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diapool::Application.config.secret_token = 'de43ffd5e2f9ae997a93a9d724b9ace513b3617166316d4f35cb592094057f1b68afeed1c1e2edf3d115c3ca071171ef9a645190741f911c733cb9a758bbe19d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Diapool::Application.config.session_store :cookie_store, key: '_diapool_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Diapool::Application.config.session_store :active_record_store
